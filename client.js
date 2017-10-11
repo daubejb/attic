4f2df58b147000aa924b5c8c7a8b282d592f69be
Jeffrey B. Daube <daubejb@gmail.com>
2017-10-11 12:57:05 -0400

    add unique id generator, halfway through base connection logic

--- client.js
+++ client.js
@@ -1,31 +1,140 @@
+var util = {
+  uuid: function () {
+    /*jshint bitwise:false */
+    var i, random;
+    var uuid = '';
+
+    for (i = 0; i < 32; i++) {
+      random = Math.random() * 16 | 0;
+      if (i === 8 || i === 12 || i === 16 || i === 20) {
+        uuid += '-';
+      }
+      uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+    }
+
+    return uuid;
+  }
+}
+
 var brain = {
   thoughts: [],
   addThought: function(thoughtText) {
+    var val = thoughtText.trim();
+
     this.thoughts.push({
-      thoughtText: thoughtText
+      id: util.uuid(),
+      thoughtText: val,
+      connections: []
     });
   },
   changeThought: function(position, thoughtText) {
     this.thoughts[position].thoughtText = thoughtText;
+  },
+  addConnection: function(sourcePosition, targetPosition, connectionType) {
+    var connectionTypeText = this.getConnectionText(connectionType);
+    this.thoughts[sourcePosition].connections.push(
+      {
+        direction: 'sending',
+        connectionText: connectionTypeText.sending,
+        targetPosition: targetPosition,
+        connectionType: connectionType
+      },
+      {
+        direction: 'receiving',
+        connectionText: connectionTypeText.receiving,
+        targetPosition: targetPosition,
+        connectionType: connectionType
+      }
+    );
+    // this.thoughts[targetPosition].connections.push(
+    //   {
+    //     direction: 'sending',
+    //     connectionText: connectionTypeText.sending,
+    //     targetPosition: targetPosition,
+    //     connectionType: connectionType
+    //   },
+    //   {
+    //     direction: 'receiving',
+    //     connectionText: connectionTypeText.receiving,
+    //     targetPosition: targetPosition,
+    //     connectionType: connectionType
+    //   }
+    // );  --- I WAS WORKING ON THIS
+  },
+  getConnectionText: function(connectionType) {
+    var sending = '';
+    var receiving = '';
+    switch (connectionType) {
+      case 'synonym':
+        sending = 'is synonymous to';
+        receiving = 'is synonymous to';
+        break;
+      case 'antonym':
+        sending = 'is opposite to';
+        receiving = 'is opposite to';
+        break;
+      case 'part':
+        sending = 'is a part of';
+        receiving = 'contains';
+        break;
+      case 'type':
+        sending = 'is a type of';
+        receiving = 'includes';
+        break;
+      case 'object':
+        sending = 'is used to';
+        receiving = 'with a';
+        break;
+      case 'perform':
+        sending = 'does';
+        receiving = 'is done by';
+        break;
+      case 'cause':
+        sending = 'is a cause of';
+        receiving = 'is caused by';
+        break;
+      case 'symbol':
+        sending = 'is a symbol of';
+        receiving = 'is symbolized by';
+        break;
+    }
+    var connectionTypeDirections = {
+      'sendingText': sending,
+      'receivingText': receiving
+    };
+    return connectionTypeDirections
   },
   deleteThought: function(position) {
     this.thoughts.splice(position, 1);
   }
 };
 
+
 var handler = {
   addThought: function () {
     var addThoughtTextInput = document.getElementById('addThoughtTextInput');
     brain.addThought(addThoughtTextInput.value);
-    addThoughtTextInput = '';
+    addThoughtTextInput.value = '';
     view.displayThoughts();
   },
   changeThought: function(position, thoughtText) {
     var changeThoughtPositionInput = document.getElementById('changeThoughtPositionInput');
     var changeThoughtTextInput = document.getElementById('changeThoughtTextInput');
     brain.changeThought(changeThoughtPositionInput.valueAsNumber, changeThoughtTextInput.value);
-    changeThoughtPositionInput = '';
-    changeThoughtTextInput = '';
+    changeThoughtPositionInput.value = '';
+    changeThoughtTextInput.value = '';
+    view.displayThoughts();
+  },
+  addConnection: function(sourcePosition, targetPosition, connectionType) {
+    var sourceThoughtPositionInput = document.getElementById('sourceThoughtPositionInput');
+    var targetThoughtPositionInput = document.getElementById('targetThoughtPositionInput');
+    var connectionTypeInput = document.getElementById('connectionTypeInput');
+    brain.addConnection(sourceThoughtPositionInput.valueAsNumber,
+                         targetThoughtPositionInput.valueAsNumber,
+                         connectionTypeInput.value);
+    sourceThoughtPositionInput.value = '';
+    targetThoughtPositionInput.value = '';
+    connectionTypeInput.value = '';
     view.displayThoughts();
   },
   deleteThought: function(position) {
